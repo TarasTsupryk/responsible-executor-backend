51aaf7452c6d306f02f6bc1e4c194820507f3598
unknown <taras.tsupryk@gmail.com>
2022-05-12 23:06:43 +0300

    provide access to the number of tenders

--- components/modules/middlewares/headers.middleware.js
+++ components/modules/middlewares/headers.middleware.js
@@ -1,4 +1,5 @@
 export default function headersMiddleware(req, res, next) {
   res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Expose-Headers", "numberOfTenders");
   next();
 }
