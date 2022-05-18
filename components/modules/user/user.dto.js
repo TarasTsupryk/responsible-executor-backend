ed7ad77f4f90d597f24b1d1fbb1da6e659c29c35
unknown <taras.tsupryk@gmail.com>
2022-05-19 00:08:16 +0300

    expand user dto

--- components/modules/user/user.dto.js
+++ components/modules/user/user.dto.js
@@ -2,11 +2,13 @@ class UserDto {
   user_id;
   email;
   is_active;
+  type;
 
   constructor(user) {
     this.user_id = user.user_id;
     this.email = user.email;
     this.is_active = user.is_active;
+    this.type = user.type;
   }
 }
 
