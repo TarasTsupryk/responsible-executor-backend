0d4e8cf59f548cad3898c2981a418e181612e52b
unknown <taras.tsupryk@gmail.com>
2022-06-01 00:54:34 +0300

    select user by access token

--- components/modules/user/user.service.js
+++ components/modules/user/user.service.js
@@ -1,10 +1,19 @@
 import databasePool from "../../shared/database.js";
+import TokenService from "../token/token.service.js";
 
 class UserService {
   async getAll() {
     const select_users_query = "SELECT * FROM user";
     const users = await databasePool.query(select_users_query);
     return users[0];
+  }
+
+  async getOne(accessToken) {
+    const userData = TokenService.validateAccessToken(accessToken);
+    const { user_id } = userData;
+    const select_users_query = `SELECT * FROM user WHERE user_id = ${user_id}`;
+    const users = await databasePool.query(select_users_query);
+    return users[0][0];
   }
 
   async activateOne(activation_link) {
