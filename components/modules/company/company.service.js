7ac7316c33477dc329e5fd910e4b841864c98b39
unknown <taras.tsupryk@gmail.com>
2022-06-05 16:33:49 +0300

    fix error

--- components/modules/company/company.service.js
+++ components/modules/company/company.service.js
@@ -1,7 +1,7 @@
 import databasePool from "../../shared/database.js";
 import TokenService from "../token/token.service.js";
 import CompanyValidator from "./company.validator.js";
-
+import ApiError from "../exceptions/api.error.js";
 class CompanyService {
   async getForUser(user_id) {
     const query = `SELECT * FROM provider WHERE owner_id = ${user_id}`;
@@ -12,23 +12,19 @@ class CompanyService {
   async createOne(company, accessToken) {
     const validate = CompanyValidator.validate(company);
     if (!validate) {
-      return "заповніть поля";
+      throw ApiError.badRequest("Не коректні дані");
     }
 
     const { full_name, short_name, classification, ownership, registry_code } =
       company;
 
-    try {
-      const { user_id } = TokenService.validateAccessToken(accessToken);
-      if (!user_id) return null;
-      const query = `INSERT INTO provider (provider_id, full_name, short_name, registry_code, ownership, classification, owner_id) 
+    const { user_id } = TokenService.validateAccessToken(accessToken);
+    if (!user_id) return null;
+    const query = `INSERT INTO provider (provider_id, full_name, short_name, registry_code, ownership, classification, owner_id) 
                      VALUES 
                      (provider_id, "${full_name}", "${short_name}", "${registry_code}", "${ownership}", "${classification}", ${user_id}) `;
-      const data = await databasePool.query(query);
-      return data[0];
-    } catch (e) {
-      console.log(e);
-    }
+    const data = await databasePool.query(query);
+    return data[0];
   }
 }
 
